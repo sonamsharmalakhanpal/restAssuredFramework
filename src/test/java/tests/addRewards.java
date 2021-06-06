7210174013ea328856f142032a6610bf0444cf99
sonamlakhanpal <sonam.lakhanpal@pointsbet.com>
2021-06-06 16:24:27 +0530

    included one more post request,pass body data as json object but only used when payload is small otherwise we will use pojo files

--- src/test/java/tests/addRewards.java
+++ src/test/java/tests/addRewards.java
@@ -1,26 +1,21 @@
 package tests;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.internal.support.FileReader;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-import pages.ApiPage;
 import utils.AutomationUtils;
 
 public class addRewards  {
 	
 	
 	static Response response;
-	static RequestSpecification requestSpec;
+//	static RequestSpecification requestSpec;
 	private static String authorisation;
 	private static String azureApiUrl;
 	private static String pbadminApiUrl;
@@ -47,9 +42,6 @@ public class addRewards  {
 	
 	@Test
 	public void validateAddRewards() throws IOException {
-//		FileInputStream file = new FileInputStream(System.getProperty("user.dir")+"/auuat.properties");
-//        Properties props = new Properties();
-//        props.load(file);
 		 response = RestAssured.given()
 	                .contentType(ContentType.JSON)
 	                .header("x-ms-client-principal-name", "lucky")
@@ -59,20 +51,27 @@ public class addRewards  {
 	        response.then().log().all();
 		
 	}
+	
+   
+	
+   @Test
+	public void validateRewardsWithJsonObject() {
+	    JSONObject json = new JSONObject();
+	    json.put("points", "1000");
+	    json.put("description", "testing via resassured");
+		response = RestAssured.given()
+				   .contentType(ContentType.JSON)
+				   .header("x-ms-client-principal-name", "lucky")
+				   .body(json.toString())
+				   .log().all()
+				   .post(props.getProperty("rewardLink") + "/api/pbadmin/" + "385970" + "/rewards" + props.getProperty("rewardCode"));
+		
+	}
+				   
+   
 
 	
-	
-//	@Test
-//	public static void getPersonalDetails() {
-//         response = RestAssured.given()
-//                .spec(requestSpec)
-//                .header("Authorization", authorisation)
-//                .get(azureApiUrl + pbadminApiUrl);
-//        response.then().log().all();
-//        response.then().statusCode(200);
-//        System.out.println("response status code is:"+response.getStatusCode());
-//		System.out.println("response status code is:"+response.getBody().toString());
-//    }
+   
 	
 	
 	
